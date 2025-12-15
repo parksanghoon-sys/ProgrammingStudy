90dc12e53f0d76dd6ac5bc209a559110a2960b2e
DESKTOP-L8PQMEN\User <power93266@naver.com>
2025-12-15 22:48:21 +0900

    update

---
+++ 강의/인프런/클린코더스_객체지향프로그래밍/example/src/BowlingGame/Game.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame;
+
+public class Game
+{
+    private int[] _rolls = new int[21];
+    private int currentRoll = 0;
+    private int _score = 0;
+    public int GetScore()
+    {
+        _score = 0;
+        int fistRollInFrame = 0;
+        for (int frame = 0; frame < 10; frame++)
+        {
+            if (IsSpare(fistRollInFrame))
+            {
+                _score += 10 + NextBallForSpare(fistRollInFrame);
+                fistRollInFrame += 2;
+            }
+            else if (IsStrike(fistRollInFrame))
+            {
+                _score += NextBallsForStrike(fistRollInFrame);
+                fistRollInFrame += 1;
+            }
+            else
+            {
+                _score += NextBallsForFrame(fistRollInFrame);
+                fistRollInFrame += 2;
+            }
+
+        }
+        return _score;
+    }
+
+    private int NextBallsForFrame(int fistRollInFrame)
+    {
+        return _rolls[fistRollInFrame] + _rolls[fistRollInFrame + 1];
+    }
+
+    private int NextBallsForStrike(int fistRollInFrame)
+    {
+        return 10 + _rolls[fistRollInFrame + 1] + _rolls[fistRollInFrame + 2];
+    }
+
+    private int NextBallForSpare(int fistRollInFrame)
+    {
+        return _rolls[fistRollInFrame + 2];
+    }
+
+    private bool IsStrike(int fistRollInFrame)
+    {
+        return _rolls[fistRollInFrame] == 10;
+    }
+
+    private bool IsSpare(int fistRollInFrame)
+    {
+        return _rolls[fistRollInFrame] + _rolls[fistRollInFrame + 1] == 10;
+    }
+
+    public void Roll(int pins)
+    {
+        _rolls[currentRoll ++] = pins;
+    }
+
+}
---
+++ 강의/인프런/클린코더스_객체지향프로그래밍/example/test/BowlingGame.Test/GameTest.cs
@@ -0,0 +1,79 @@
+﻿namespace BowlingGame.Test;
+
+public class GameTest
+{
+    private Game _game;
+    public GameTest()
+    {
+        _game = new Game();
+    }
+    private void RollMany(int pins, int frames)
+    {
+        for (int i = 0; i < frames; i++)
+            _game.Roll(pins);
+    }
+    private void RollSpare()
+    {
+        _game.Roll(5);
+        _game.Roll(5);
+    }
+    [Fact]
+    public void CanRoll()
+    {
+        _game.Roll(0);
+    }
+    [Fact]
+    public void GutterGame()
+    {        
+        RollMany(0, 20);
+        Assert.Equal(0, _game.GetScore());
+    }
+    [Fact]
+    public void AllOnesGame()
+    {
+        RollMany(1, 20);
+        Assert.Equal(20, _game.GetScore());
+    }
+    [Fact]
+    public void OneSpare()
+    {
+        RollSpare();
+        _game.Roll(3);
+        RollMany(17, 0);
+        Assert.Equal(16, _game.GetScore());
+    }
+    [Fact]
+    public void OneStrike()
+    {
+        RollStrike();
+        _game.Roll(5);
+        _game.Roll(3);
+        RollMany(16, 0);
+        Assert.Equal(26, _game.GetScore());
+    }
+    [Fact]
+    public void PerfectGame()
+    {        
+        RollMany(10, 10);
+        RollStrike();
+        RollStrike();
+        Assert.Equal(300, _game.GetScore());
+    }
+    [Fact(Skip = "스킵 방법예시")]
+    public void SkippedTestExample()
+    {
+        // This test is skipped.
+    }
+    [Theory(Skip = "버그 수정 대기")]
+    [InlineData(1, 2)]
+    [InlineData(3, 4)]
+    public void Add_test(int a, int b)
+    {
+        Assert.True(true);
+    }
+    private void RollStrike()
+    {
+        _game.Roll(10);
+    }
+}
+
